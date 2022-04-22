7aeddeec070e9f51a6a82bfca66b0c0fe820135c
1298857 <89183458+1298857@users.noreply.github.com>
2022-04-22 08:37:28 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,8 @@
+resource "aws_instance" "instance1" {
+  ami = data.aws_ami.ami.id
+  instance_type = var.instance_type
+  key_name = var.key_name
+  tags = {
+    "Name" = var.Name
+  }
+}
---
+++ vars.tf
@@ -0,0 +1,10 @@
+variable "instance_type" {
+    type = string
+    default = "t2.micro"  
+}
+variable "key_name" {
+    default = "ssh"
+}
+variable "Name" {
+  default = "EC2"
+}
